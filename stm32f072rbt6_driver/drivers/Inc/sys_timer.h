6565a5f44d61dd57aa0860d70250e1fb1a200067
milanbijeljac <95877861+milanbijeljac@users.noreply.github.com>
2024-11-13 23:10:09 +0100

    Adding default template.

--- stm32f072rbt6_driver/drivers/Inc/sys_timer.h
+++ stm32f072rbt6_driver/drivers/Inc/sys_timer.h
@@ -1,90 +1,71 @@
-/*
- * sys_delay.h
- *
- *  Created on: Jan 21, 2024
- *      Author: Milan
- */
-
 #ifndef INC_SYS_TIMER_H_
 #define INC_SYS_TIMER_H_
 
+/* **************************************************
+ *			    	INCLUDES					    *
+ *************************************************  */
+
 #include <stm32f072x8.h>
 #include <types.h>
+
+/* **************************************************
+ *					DEFINES 					    *
+ *************************************************  */
 
 #define US_TO_MS 1000u
 #define MS_TO_S  1000u
 
 /**
- * @fn				- Function used to configure general purpose timer TIM3.
  *
- * @brief			- This function enables or disables peripheral clock for the given GPIO port
- *
- * @param[in]		- GPIO_TypeDef *pGPIOx - base address of GPIOx port
- * @param[in]		- uint8 enabledOrDisabled - or disable port
- *
- * @return			-
- *
- * @Note			-
+ * \brief      - TODO
+ * \param[in]  - TODO
+ * \param[out] - TODO
+ * \return     - TODO
+ * \globals    - TODO
  *
  */
 void TIM3_v_cfg(uint8 u_frequency);
 
 /**
- * @fn				- Delay_v_us
  *
- * @brief			- This function enables or disables peripheral clock for the given GPIO port
- *
- * @param[in]		- GPIO_TypeDef *pGPIOx - base address of GPIOx port
- * @param[in]		- uint8 enabledOrDisabled - or disable port
- *
- * @return			-
- *
- * @Note			-
+ * \brief      - TODO
+ * \param[in]  - TODO
+ * \param[out] - TODO
+ * \return     - TODO
+ * \globals    - TODO
  *
  */
 void Delay_v_us (uint16 us);
 
 /**
- * @fn				- Delay_v_ms
  *
- * @brief			- This function enables or disables peripheral clock for the given GPIO port
- *
- * @param[in]		- GPIO_TypeDef *pGPIOx - base address of GPIOx port
- * @param[in]		- uint8 enabledOrDisabled - or disable port
- *
- * @return			-
- *
- * @Note			-
+ * \brief      - TODO
+ * \param[in]  - TODO
+ * \param[out] - TODO
+ * \return     - TODO
+ * \globals    - TODO
  *
  */
 void Delay_v_ms (uint16 ms);
 
 /**
- * @fn				- Delay_v_seconds
  *
- * @brief			- This function enables or disables peripheral clock for the given GPIO port
- *
- * @param[in]		- GPIO_TypeDef *pGPIOx - base address of GPIOx port
- * @param[in]		- uint8 enabledOrDisabled - or disable port
- *
- * @return			-
- *
- * @Note			-
+ * \brief      - TODO
+ * \param[in]  - TODO
+ * \param[out] - TODO
+ * \return     - TODO
+ * \globals    - TODO
  *
  */
 void Delay_v_seconds(uint16 seconds);
 
 /**
- * \fn				- Delay_v_seconds
  *
- * \brief			- This function enables or disables peripheral clock for the given GPIO port
- *
- * \param[in]		- GPIO_TypeDef *pGPIOx - base address of GPIOx port
- * \param[in]		- uint8 enabledOrDisabled - or disable port
- *
- * \return			-
- *
- * \Note			-
+ * \brief      - TODO
+ * \param[in]  - TODO
+ * \param[out] - TODO
+ * \return     - TODO
+ * \globals    - TODO
  *
  */
 void TIM2_v_IrFrequencyCfg(void);
