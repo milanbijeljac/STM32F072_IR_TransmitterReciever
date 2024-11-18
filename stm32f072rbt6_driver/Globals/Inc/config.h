eda4e3d325e73f94525c34462f0b6bc2cd182db7
milanbijeljac <95877861+milanbijeljac@users.noreply.github.com>
2024-11-18 22:48:54 +0100

    Configuration update for ZHJT-03 IR receiving.

--- stm32f072rbt6_driver/Globals/Inc/config.h
+++ stm32f072rbt6_driver/Globals/Inc/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H_
 
 #define DEBUGG_MODE  	STD_ON
-#define NEC_PROTOCOL 	STD_ON
-#define ZHJT03_REMOTE	STD_OFF
+#define NEC_PROTOCOL 	STD_OFF
+#define ZHJT03_REMOTE	STD_ON
 
 #endif /* CONFIG_H_ */
